9bc290cfa78355f7faa64e14b5d02d1873706c0d
amitchew <49229715+amitchew@users.noreply.github.com>
2023-01-17 19:45:24 +0300

    Update find-nearest-point-that-has-the-same-x-or-y-coordinate.py

--- find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,11 +1,11 @@
 class Solution:
    def nearestValidPoint(self, x, y, points):
-        init_d=float('inf')
         ans=-1
+        val=float('inf')
         for i in range(len(points)):
             a, b = points[i]
             if a==x or b==y:
                 d = abs(a-x) + abs(b-y)
                 if d < init_d:
-                    init_d, ans = d, i
+                    val, ans = d, i
         return ans
