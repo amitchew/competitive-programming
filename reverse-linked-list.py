c09d910f04134f4f5c23c2eb978e18130b00db83
amitchew <49229715+amitchew@users.noreply.github.com>
2023-02-23 21:24:05 +0300

    Create reverse-linked-list.py

---
+++ reverse-linked-list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        arr=[]
+
+        while head:
+            arr.append(head.val)
+            head=head.next
+        
+        if arr==arr[::-1]:
+            return True
+            
+        else:
+            return False
